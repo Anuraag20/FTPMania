3028f995f06068f332a381325abce2d76a4626dc
Anuraag20 <amukh20@gmail.com>
2021-09-29 04:47:14 +0530

    for real this time

--- frontend/src/components/RoomComponents/Room.js
+++ frontend/src/components/RoomComponents/Room.js
@@ -136,7 +136,7 @@ const Room = (props) =>{
     getTimeLeft();
     getFiles();
 
-    client = new W3Cwebsocket('ws://'+ window.location.host +'/ws/room/' + roomCode +':8001/')
+    client = new W3Cwebsocket('ws://'+ window.location.host +':8001/ws/room/' + roomCode +'/')
     
     client.onmessage = (receivedData) => {
       
