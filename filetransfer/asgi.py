1a798e4801155402bc75bd76b5da065e6045d32f
Anuraag20 <amukh20@gmail.com>
2021-09-29 05:08:15 +0530

    sad

--- filetransfer/asgi.py
+++ filetransfer/asgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 import django
 from decouple import config
-# from channels.routing import get_default_application
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{config("PROJECT_NAME")}.settings')
 django.setup()
-application = get_asgi_application()
-# application = get_default_application()
+# application = get_asgi_application()
+application = get_default_application()
