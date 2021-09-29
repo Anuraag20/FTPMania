b9f09c9b8f596bb16d169b8a6a63410d5eb25cb4
Anuraag20 <amukh20@gmail.com>
2021-09-29 06:21:42 +0530

    why not

--- filetransfer/settings.py
+++ filetransfer/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['139.59.30.56', '*']
+ALLOWED_HOSTS = ['139.59.30.56']
 
 
 # Application definition
